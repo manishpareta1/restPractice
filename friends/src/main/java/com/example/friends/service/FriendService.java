543c05af2dc5874912996071e4fe8a65d22b87a8
manishpareta1 <manishpareta1@gmail.com>
2019-09-11 18:12:43 +0530

    added validation exception for all operations

--- friends/src/main/java/com/example/friends/service/FriendService.java
+++ friends/src/main/java/com/example/friends/service/FriendService.java
@@ -3,9 +3,11 @@ package com.example.friends.service;
 import com.example.friends.model.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface FriendService extends JpaRepository<Friend, Integer> {
 
-    public Iterable<Friend> findByFirstNameAndLastName(String firstName, String lastName);
-    public Iterable<Friend> findByFirstName(String firstName);
-    public Iterable<Friend> findByLastName(String lastName);
+    public Collection<Friend> findByFirstNameAndLastName(String firstName, String lastName);
+    public Collection<Friend> findByFirstName(String firstName);
+    public Collection<Friend> findByLastName(String lastName);
 }
