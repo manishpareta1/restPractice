14b0ccf307f2fcc771b727a6ac97faef3edf7806
manishpareta1 <manishpareta1@gmail.com>
2019-09-13 15:43:03 +0530

    added Unit and System test cases

--- friends/src/test/java/com/example/friends/FriendsApplicationTests.java
+++ friends/src/test/java/com/example/friends/FriendsApplicationTests.java
@@ -1,16 +1,33 @@
 package com.example.friends;
 
+import com.example.friends.controller.FriendController;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * @RunWith tells that is uses Spring Junti test api for test case
+ * @SpringBootTest tells the spring boot application will start inside a test
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FriendsApplicationTests {
 
+	//Spring Context will instantiate a FriendControlle and inject it.
+	@Autowired
+	FriendController friendController;
+
+	/**
+	 * contextLoads will validate if the spring context
+	 * is initialized and bean is instantiated and injected
+	 * @Test tells its this method will run when the unit test is run
+	 */
 	@Test
 	public void contextLoads() {
+		Assert.assertNotNull(friendController);
 	}
 
 }
--- friends/src/test/java/com/example/friends/SystemTest.java
+++ friends/src/test/java/com/example/friends/SystemTest.java
@@ -1,4 +1,29 @@
 package com.example.friends;
 
+import com.example.friends.model.Friend;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class SystemTest {
+
+    @Test
+    public void createReadDelete(){
+        RestTemplate restTemplate = new RestTemplate();
+
+        Friend friend = new Friend(0,"Adam5","Gilchrist",40,null);
+        String url = "http://localhost:8082/friends";
+
+        ResponseEntity<Friend> entity = restTemplate.postForEntity(url+"/add", friend, Friend.class);
+
+        Friend[] friends = restTemplate.getForObject(url, Friend[].class);
+        Assertions.assertThat(friends).extracting(Friend::getFirstName).contains("Adam5");
+
+        restTemplate.delete(url+"/delete/"+entity.getBody().getId());
+        friends = restTemplate.getForObject(url, Friend[].class);
+        Assertions.assertThat(friends).extracting(Friend::getFirstName).doesNotContain("Adam5");
+
+    }
+
 }
