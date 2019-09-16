204f0540ca86447437391e1deb4b0bd4b91c6eb1
manishpareta1 <manishpareta1@gmail.com>
2019-09-16 12:51:57 +0530

    added mockito test case for controller test

---
+++ friends/src/test/java/com/example/friends/FriendControllerMockTest.java
@@ -0,0 +1,73 @@
+package com.example.friends;
+
+
+import com.example.friends.controller.FriendController;
+import com.example.friends.model.Address;
+import com.example.friends.model.Friend;
+import com.example.friends.service.FriendService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import org.mockito.Mockito;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import static org.hamcrest.Matchers.*;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+//Allows us to call urls in the test context will not make actual call
+@WebMvcTest(FriendController.class)
+public class FriendControllerMockTest {
+
+    //we are mocking friendservice here, this means a dummy service to perform operations.
+    @MockBean
+    FriendService friendService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testCreateReadDelete(){
+
+
+        List<Address> addressList = Arrays.asList(new Address(0,"Adam5 Street", "Adam5 City"));
+        List<Friend> friendList = Arrays.asList(new Friend(0,"Adam5","Gilchrist",40,addressList));
+
+        Mockito.when(friendService.findAll()).thenReturn(friendList);
+        try {
+            mockMvc.perform(get("/friends"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$", hasSize(1)))
+                    .andExpect(jsonPath("$[0].first-name", is("Adam5")));
+            verify(friendService, Mockito.times(1)).findAll();
+            verifyNoMoreInteractions(friendService);
+        }catch(Exception e ){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
+}
